139013a7d6d9a2dda0176df6b9a0234d49b6ba82
Damarice <menganyidamarice@gmail.com>
2024-09-25 09:12:08 +0300

    new

--- config/db.js
+++ config/db.js
@@ -1,18 +1,15 @@
-// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async (uri) => {
     try {
         await mongoose.connect(uri, {
             useNewUrlParser: true, // Optional for older versions of MongoDB
-            useUnifiedTopology: true, // Optional for older versions of MongoDB
-            useFindAndModify: false, // Disable findAndModify deprecation warning
-            useCreateIndex: true // Ensure indexes are created
+            useUnifiedTopology: true // Optional for older versions of MongoDB
         });
         console.log('MongoDB connected successfully!');
     } catch (error) {
         console.error('MongoDB connection error:', error.message);
-        process.exit(1); // Exit the process with failure
+        process.exit(1);
     }
 };
 
