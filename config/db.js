6a3f1175ede67292e1316a4d21ed5cc3db81c6f3
Damarice <menganyidamarice@gmail.com>
2024-09-25 09:46:12 +0300

    nn

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async (uri) => {
     try {
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(uri); // Removed deprecated options
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection error:', error);
